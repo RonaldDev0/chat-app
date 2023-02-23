7102b4d74c2ea58cc5e67a1627b78064c5d2afd8
Ronald <ronaldzamoradev@gmail.com>
2023-02-22 19:18:22 -0500

    .

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -3,20 +3,13 @@ import NavBarr from './NavBarr'
 import FormMessage from './FormMessage'
 import ChatHistory from './ChatHistory'
 
-import style from './scss/Index.module.scss'
+import style from './scss/index.module.scss'
 
 export default function Chat ({ User, chatOpen, messages }: { User: String | null, chatOpen: String | null, messages: any }) {
   return (
     <div className={style.container}>
       {
-      !chatOpen
-        ? <BackgroundDefault />
-        :
-        <>
-          <NavBarr chatOpen={chatOpen} />
-          <ChatHistory chatOpen={chatOpen} messages={messages} User={User} />
-          <FormMessage chatOpen={chatOpen} User={User} />
-        </>
+      !chatOpen ? <BackgroundDefault /> : <><NavBarr chatOpen={chatOpen} /><ChatHistory chatOpen={chatOpen} messages={messages} User={User} /><FormMessage chatOpen={chatOpen} User={User} /></>
       }
     </div>
   )
